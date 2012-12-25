f90bd9682cae3d02cc63e160601f120a34ae3456
Ladislav Lisy <ladislav.lisy@seznam.cz>
2012-12-25 12:49:22 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HraveMzdyJaro::Application.config.secret_token = '74df49ae0f3dc51879afaca0b2b21c5aeda3d2233055aeb96fd00a092b8148bc3218dc6e5b175320e1cde684426ceb426e6f6ff0e700c34adaabc2719d7025ba'
